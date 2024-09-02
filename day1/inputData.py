5fb4f055b86186c27a5ab2a0467df1ec77f06c4f
yabsiraditya <yabsir662@gmail.com>
2024-09-02 22:31:02 +0700

    day-1 input data

---
+++ day1/inputData.py
@@ -0,0 +1,18 @@
+# Input User
+
+# Data yang di masukan pasti String
+data = input("Masukan Data : ")
+
+print("Data = ", data, type(data))
+
+# Data yang di masukan harus Integer/Float
+number = int(input("Masukan Angka : "))
+number = float(input("Masukan Angka : "))
+
+
+print("Data = ", number, type(number))
+
+# Data yang di masukan harus 0/1 jika Boolean
+biner = bool(int(input("Masukan Nilai Boolean 0/1 = ")))
+
+print("Data = ", biner, type(biner))
