ae8a7a92321b37e70ab51a961787080fbdcbb791
yabsiraditya <yabsir.aditya@gmail.com>
2024-09-22 15:51:17 +0700

    update OOP python

--- OOP/day1/latihan.py
+++ OOP/day1/latihan.py
@@ -1,24 +1,30 @@
 class Heroes:   # Template
 
-    def __init__(self,inputName,inputHealth,inputPower,inputArmor):
+    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
         # instance variable
         self.name = inputName
         self.health = inputHealth
         self.power = inputPower
         self.armor = inputArmor
     
-    def attack(self,attack):
+    def attack(self, attack):
         print(f"{self.name} Menyerang {attack.name}")
-        attack.underAttack(self)
+        attack.underAttack(self, self.power)
 
-    def underAttack(self,attack):
+    def underAttack(self, attack, attackPowerEnemy):
         print(f"{self.name} Diserang {attack.name}")
+        attackReceived = attackPowerEnemy/self.armor
+        print(f"Serangan Diterima : {str(attackReceived)}")
+        self.health -= attackReceived
+        print(f"Darah {self.name} Tersisa {str(self.health)}")
 
 
-sniper = Heroes("Sniper",100,25,4)
-mirana = Heroes("Mirana",220,50,7)
+sniper = Heroes("Sniper",120,10,5)
+mirana = Heroes("Mirana",100,20,10)
 asep = Heroes("Asep",550,30,9)
 udin = Heroes("Udin",330,10,10)
 
 
-sniper.attack(asep)
+sniper.attack(mirana)
+print("\n")
+mirana.attack(sniper)
